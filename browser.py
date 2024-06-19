9b27f0e18c3ff0c5164588a82b207e6467533bf5
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2024-06-19 15:20:43 +0500

    Update browser.py

--- browser.py
+++ browser.py
@@ -10,17 +10,13 @@ from tls_client.exceptions import TLSClientExeption
 
 
 class Browser:
-    def __init__(self):
-
-        if PROXY not in ['http://log:pass@ip:port', '']:
-            self.change_ip()
-
+    def __init__(self, proxy=None):
+        self.proxy = proxy
         self.session = self.get_new_session()
         self.session.headers.update({
             "Origin": None,
             "Referer": None,
         })
-
 
     def get_new_session(self):
         session = Session(
@@ -29,34 +25,10 @@ class Browser:
         )
         session.headers['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
-        if PROXY not in ['http://log:pass@ip:port', '']:
-            session.proxies.update({'http': PROXY, 'https': PROXY})
+        if self.proxy:
+            session.proxies.update({'http': self.proxy, 'https': self.proxy})
 
         return session
-
-
-    def change_ip(self):
-        if CHANGE_IP_LINK not in ['https://changeip.mobileproxy.space/?proxy_key=...&format=json', '']:
-            while True:
-                try:
-                    r = get(CHANGE_IP_LINK)
-                    if 'mobileproxy' in CHANGE_IP_LINK and r.json().get('status') == 'OK':
-                        print('') # empty string before next acc
-                        logger.debug(f'[+] Proxy | Successfully changed ip: {r.json()["new_ip"]}')
-                        return True
-                    elif not 'mobileproxy' in CHANGE_IP_LINK and r.status_code == 200:
-                        print('') # empty string before next acc
-                        logger.debug(f'[+] Proxy | Successfully changed ip: {r.text}')
-                        return True
-                    logger.error(f'[-] Proxy | Change IP error: {r.text} | {r.status_code}')
-                    sleep(10)
-
-                except TLSClientExeption as err:
-                    logger.error(f'[-] Browser | {err}')
-
-                except Exception as err:
-                    logger.error(f'[-] Browser | Cannot get proxy: {err}')
-
 
     def get_eligibility(self, address: str, retry: int = 0):
         try:
@@ -76,11 +48,9 @@ class Browser:
             return status, tokens
 
         except Exception as err:
-            logger.error(f"[-] Browser | Coudlnt get eligibility: {err} [{retry + 1}/{RETRY}]")
+            logger.error(f"[-] Browser | Couldn't get eligibility: {err} [{retry + 1}/{RETRY}]")
             if retry < RETRY:
                 sleep(3)
                 return self.get_eligibility(address=address, retry=retry + 1)
             else:
-                return "Coudlnt get eligibility", 0
-
-
+                return "Couldn't get eligibility", 0
