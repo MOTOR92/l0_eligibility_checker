e7f55640f2f559ceae55270647e6de58e6697d23
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2024-06-19 15:22:34 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,1 @@
-
-# МОБИЛЬНЫЕ ПРОКСИ
-PROXY           = 'http://log:pass@ip:port'                                         # что бы не использовать прокси - оставьте как есть
-CHANGE_IP_LINK  = 'https://changeip.mobileproxy.space/?proxy_key=...&format=json'   # для резидентских прокси - тут оставьте как есть
-
 RETRY           = 3                                                                 # количество повторов при ошибках
